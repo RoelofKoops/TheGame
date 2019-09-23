5f17ec79f967e6dfd68368f793e4cccbcb3f2248
roel0Fzo <99055522@mydavinci.nl>
2019-09-23 13:45:01 +0200

    Begonnen aan de game

---
+++ thegame.js
@@ -0,0 +1,6 @@
+
+alert ('welkom in deze game, ik ben de spelleider en zal je even uitleggen hoe spel dit werkt.')
+alert ('je krijgt straks steeds vragen die je moet beantwoorden met ja, nee of een cijfer. LET OP!! DEZE KEUZES BEINVLOEDEN DE REST VAN HET SPEL DENK DUS GOED NA OVER DEZE KEUZES! (je kan het spel meerdere keren spelen.)')
+alert ('het spel begint nu')
+alert ('')
+	
